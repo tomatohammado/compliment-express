e49c1604e8820e009a2b88321d5e3b5e6ceff979
Hammad Malik <hmd.node@gmail.com>
2017-12-18 18:37:23 -0500

    Add compliments and colors array
    Used the assignment page for the content, no changes.
    
    Yet.

---
+++ index.js
@@ -0,0 +1,14 @@
+compliments = [
+  'Your instructors love you',
+  'High five = ^5',
+  'Is it Ruby Tuesday yet?',
+  'It\'s almost party o\'clock',
+  'The Force is strong with you'
+]
+
+colors = [
+  '#FFBF00',
+  '#0080FF',
+  '#01DF3A',
+  '#FF0080'
+]
